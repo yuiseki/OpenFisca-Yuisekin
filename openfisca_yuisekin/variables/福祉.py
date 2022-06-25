07476fc384fd9faa286bcaded571ea90f8fe581d
yuiseki <yuiseki@gmail.com>
2022-06-25 15:36:20 +0900

    児童手当

--- openfisca_yuisekin/variables/福祉.py
+++ openfisca_yuisekin/variables/福祉.py
@@ -13,6 +13,8 @@ from openfisca_core.variables import Variable
 # Import the Entities specifically defined for this tax and benefit system
 from openfisca_yuisekin.entities import 世帯, 人物
 
+import numpy as np
+
 
 class ベーシックインカム(Variable):
     value_type = float
@@ -71,6 +73,38 @@ class 年金(Variable):
         年齢条件 = 対象人物("年齢", 対象期間) >= parameters(対象期間).全般.定年年齢
         return 年齢条件
 
+class 児童手当(Variable):
+    value_type = float
+    entity = 世帯
+    definition_period = MONTH
+    label = "保護者への児童手当"
+    documentation = "実際の渋谷区の制度を参考にしている"
+    reference = "https://www.city.shibuya.tokyo.jp/kodomo/teate/teate/jido_t.html"
+    #contact_address = "〒150-8010 東京都渋谷区宇田川町1-1"
+    #contact_window = "渋谷区子ども青少年課子育て給付係"
+    #contact_email = "児童手当@shibuya.tokyo.metro.jp"
+    #contact_tel = "03-3463-2558"
+
+    def formula(対象世帯, 対象期間, parameters):
+        児童手当 = parameters(対象期間).福祉.児童手当
+
+        世帯高所得 = 対象世帯("世帯高所得", 対象期間)
+        # TODO
+        所得上限限度額 = 児童手当.所得上限限度額.扶養人数1人
+        所得条件 = 世帯高所得 <= 所得上限限度額
+
+        児童一覧の年齢 = 対象世帯.members("年齢", 対象期間)
+        三歳未満の児童の人数 = 対象世帯.sum(児童一覧の年齢 < 3)
+        三歳から小学校修了前の児童の人数 = 対象世帯.sum(児童一覧の年齢 < 13) - 三歳未満の児童の人数
+        中学生の児童の人数 = 対象世帯.sum(児童一覧の年齢 < 16) - 三歳から小学校修了前の児童の人数 - 三歳未満の児童の人数
+
+        手当条件 = 所得条件 * 三歳未満の児童の人数
+        手当金額 = \
+            + (児童手当.金額.三歳未満 * 三歳未満の児童の人数) \
+            + (児童手当.金額.三歳から小学校修了前 * 三歳から小学校修了前の児童の人数) \
+            + (児童手当.金額.中学生 * 中学生の児童の人数)
+
+        return 手当条件 * 手当金額
 
 class 児童扶養手当(Variable):
     value_type = float
@@ -108,3 +142,14 @@ class 世帯所得(Variable):
         """世帯全員の収入"""
         各収入 = 対象世帯.members("所得", 対象期間)
         return 対象世帯.sum(各収入)
+
+class 世帯高所得(Variable):
+    value_type = float
+    entity = 世帯
+    definition_period = MONTH
+    label = "The sum of the salaries of those living in a 世帯"
+
+    def formula(対象世帯, 対象期間, _parameters):
+        """世帯全員の収入"""
+        各収入 = 対象世帯.members("所得", 対象期間)
+        return 対象世帯.max(各収入)
