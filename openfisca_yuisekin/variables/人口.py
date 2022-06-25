e9997a08f36d4dbef2a37a6b9346adfd04425a98
WataruNishimura <wataru.nishimura@kraftsman.jp>
2022-06-25 15:34:58 +0900

    生存状況に関するVariablesを追加

--- openfisca_yuisekin/variables/人口.py
+++ openfisca_yuisekin/variables/人口.py
@@ -6,13 +6,16 @@ A variable is a property of an Entity such as a 人物, a 世帯…
 See https://openfisca.org/doc/key-concepts/variables.html
 """
 
+from cProfile import label
 from datetime import date
+from xmlrpc.client import Boolean
 
 # Import from numpy the operations you need to apply on OpenFisca's population vectors
 # Import from openfisca-core the Python objects used to code the legislation in OpenFisca
 from numpy import where
 from openfisca_core.periods import ETERNITY, MONTH, DAY
 from openfisca_core.variables import Variable
+from openfisca_core.indexed_enums import Enum
 
 # Import the Entities specifically defined for this tax and benefit system
 from openfisca_yuisekin.entities import 人物
@@ -27,6 +30,11 @@ class 誕生年月日(Variable):
     definition_period = ETERNITY  # This variable cannot change over time.
     reference = "https://en.wiktionary.org/wiki/birthdate"
 
+class 死亡年月日(Variable):
+    value_type = date
+    entity = 人物
+    label = "人物の死亡年月日"
+    definition_period = ETERNITY  # This variable cannot change over time.
 
 class 年齢(Variable):
     value_type = int
@@ -64,3 +72,32 @@ class 学年(Variable):
         print(学年を繰り上げるべき)
 
         return (対象期間.start.year - 誕生年) + where(学年を繰り上げるべき, 1, 0)
+
+class 行方不明(Variable):
+    value_type = bool
+    entity = 人物
+    definition_period = DAY
+    label = "行方不明"
+
+class 生存状態(Enum):
+    __order__ = "生存　死亡　不明"
+    生存 = "生存"
+    死亡 = "死亡"
+    不明 = "不明"
+
+class 生存状況(Variable):
+    value_type = Enum 
+    possible_values = 生存状態
+    default_value = 生存状態.生存
+    entity = 人物
+    definition_period = DAY
+    label = "生存状況"
+
+    def formula(対象人物, 対象期間):
+        誕生年月日 = 対象人物("誕生年月日", 対象期間)
+        死亡年月日 = 対象人物("死亡年月日", 対象期間)
+        死亡 = (死亡年月日 != None) * (死亡年月日 > 誕生年月日) * (誕生年月日 < 対象期間.start)
+
+        生存状況 = where(死亡, 生存状態.死亡, 生存状態.生存)
+
+        return where(行方不明, 生存状態.不明, 生存状況 )
