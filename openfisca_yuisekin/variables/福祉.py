0f23653a937e830c9ffb0e11763c0e05e5bb319d
yuiseki <yuiseki@gmail.com>
2022-06-26 12:07:15 +0900

    tweak document

--- openfisca_yuisekin/variables/福祉.py
+++ openfisca_yuisekin/variables/福祉.py
@@ -79,12 +79,14 @@ class 児童手当(Variable):
     entity = 世帯
     definition_period = MONTH
     label = "保護者への児童手当"
-    documentation = "実際の渋谷区の制度を参考にしている"
     reference = "https://www.city.shibuya.tokyo.jp/kodomo/teate/teate/jido_t.html"
-    #contact_address = "〒150-8010 東京都渋谷区宇田川町1-1"
-    #contact_window = "渋谷区子ども青少年課子育て給付係"
-    #contact_email = "児童手当@shibuya.tokyo.metro.jp"
-    #contact_tel = "03-3463-2558"
+    documentation = """
+    渋谷区の児童手当制度
+    
+    - 〒150-8010 東京都渋谷区宇田川町1-1
+    - 渋谷区子ども青少年課子育て給付係
+    - 03-3463-2558
+    """
 
     def formula(対象世帯, 対象期間, parameters):
         児童手当 = parameters(対象期間).福祉.児童手当
