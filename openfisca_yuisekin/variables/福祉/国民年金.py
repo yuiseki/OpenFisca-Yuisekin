3e427d558d0d122276b008927facb931dc41c38c
yuiseki <yuiseki@gmail.com>
2022-07-02 11:39:00 +0900

    実装できた

--- openfisca_yuisekin/variables/福祉/国民年金.py
+++ openfisca_yuisekin/variables/福祉/国民年金.py
@@ -10,13 +10,7 @@ from openfisca_core.indexed_enums import Enum
 from openfisca_core.periods import ETERNITY
 from openfisca_core.variables import Variable
 from openfisca_yuisekin.entities import 人物
-
-
-class 身体障害者手帳最新交付年月日(Variable):
-    value_type = date
-    entity = 人物
-    label = "人物の身体障害者手帳の最新交付年月日"
-    definition_period = ETERNITY
+from numpy import asarray, select
 
 
 class 国民年金被保険者パターン(Enum):
@@ -36,8 +30,10 @@ class 国民年金被保険者(Variable):
     label = "人物の国民年金被保険者"
 
     def formula(対象人物, 対象期間, parameters):
-      年齢 = 対象人物("年齢", 対象期間)
-      開始年齢 = parameters(対象期間).福祉.第1号被保険者.開始年齢
-      終了年齢 = parameters(対象期間).福祉.第1号被保険者.終了年齢
-      条件 = (開始年齢 <= 年齢) * (年齢 < 終了年齢)
-      return (条件 * 国民年金被保険者パターン.第1号被保険者)
+        年齢 = 対象人物("年齢", 対象期間)
+        開始年齢 = parameters(対象期間).福祉.国民年金.第1号被保険者.開始年齢
+        終了年齢 = parameters(対象期間).福祉.国民年金.第1号被保険者.終了年齢
+        国民年金被保険者条件 = (開始年齢 <= 年齢) * (年齢 < 終了年齢)
+        パターン = 国民年金被保険者パターン.第1号被保険者 if 国民年金被保険者条件 else 国民年金被保険者パターン.無
+        return 国民年金被保険者パターン.encode(asarray([パターン]))[0]
+
